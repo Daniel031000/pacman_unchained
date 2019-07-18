139e4c89f60b5964f78811de321a186ab3c377dc
Daniel031000 <52997667+Daniel031000@users.noreply.github.com>
2019-07-18 12:23:39 +0200

    öhvjöhv

--- gamePlatform.py
+++ gamePlatform.py
@@ -9,3 +9,16 @@ class gamePlatform:
     def __init__(self, startPos, hitBox):
         self.position = startPos
         self.hitBox = hitBox
+
+##############
+#Enemy der zufällig läuft
+#############
+import random
+class Geist:
+    def Grundstock(self):
+        self.movementDirection=[0,0]
+        self.movementSpeed=5
+    def Moving(self):
+        self.movementDirection[0] = random(-1,1)
+        self.movementDirection[1] = random(-1,1)
+    def
